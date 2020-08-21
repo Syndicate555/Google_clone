362c770154725bfdffbe9f177e8561ee4c71402d
Saffat Aziz <saziz028@uottawa.ca>
2020-08-21 14:52:53 -0400

    changes

--- googleclone/src/App.js
+++ googleclone/src/App.js
@@ -5,12 +5,21 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 function App() {
   return (
     //BEM
-    <div >
-     <h1>Lets build the google clone</h1>
-         {/* Home page */}
-         {/* Search page */}
-      <Home/>
+    <div className = "app">
+    <Router>
+     <Switch>
+       <Route path = "/search">
+       {/* <SearchPage/> */}
+       <h1>This is the search page</h1>
+       </Route>
+       <Route path = "/">
+       <Home/>
+       <h1>This is the home page</h1>
+       </Route>
+     </Switch>
+      </Router>
     </div>
+  
 
   );
 }
--- googleclone/src/pages/Home.js
+++ googleclone/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Home.css'
+import {Link} from 'react-router-dom'
 function Home() {
     return (
         <div className ="home">
             <h1>This is Homepage</h1>
             <div className = "home__header">
-            <div className = "home__headerLeft"></div>
+            <div className = "home__headerLeft">
+            <Link to = "about">About</Link>
+            </div>
             <div className = "home__headerRight"></div>
             </div>
             <div className = "home__body"></div>
