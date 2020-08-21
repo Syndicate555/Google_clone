d1375150d682ac993dae8a4059f372fb711de2a7
Saffat Aziz <saziz028@uottawa.ca>
2020-08-21 01:35:59 -0400

    changes

--- googleclone/src/pages/Home.js
+++ googleclone/src/pages/Home.js
@@ -4,7 +4,10 @@ function Home() {
     return (
         <div className ="home">
             <h1>This is Homepage</h1>
-            <div className = "home__header"></div>
+            <div className = "home__header">
+            <div className = "home__headerLeft"></div>
+            <div className = "home__headerRight"></div>
+            </div>
             <div className = "home__body"></div>
         </div>
     )
