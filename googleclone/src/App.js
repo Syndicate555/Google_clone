72e2ff5138e11c7ed50355b6504b1842aa44920b
Saffat Aziz <saziz028@uottawa.ca>
2020-08-21 01:33:47 -0400

    changes

--- googleclone/src/App.js
+++ googleclone/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import Home from "./pages/Home"
 function App() {
   return (
     //BEM
@@ -8,7 +8,7 @@ function App() {
      <h1>Lets build the google clone</h1>
          {/* Home page */}
          {/* Search page */}
-
+      <Home/>
     </div>
 
   );
--- googleclone/src/pages/Home.js
+++ googleclone/src/pages/Home.js
@@ -3,7 +3,9 @@ import './Home.css'
 function Home() {
     return (
         <div className ="home">
-            
+            <h1>This is Homepage</h1>
+            <div className = "home__header"></div>
+            <div className = "home__body"></div>
         </div>
     )
 }
