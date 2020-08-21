adc5cd3c055cfc8305fff728e39c37a496d3f4bc
Saffat Aziz <saziz028@uottawa.ca>
2020-08-21 01:27:53 -0400

    changes

--- googleclone/src/App.js
+++ googleclone/src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 function App() {
   return (
+    //BEM
     <div >
      <h1>Lets build the google clone</h1>
+         {/* Home page */}
+         {/* Search page */}
+
     </div>
+
   );
 }
 
