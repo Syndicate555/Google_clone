b49a3ece99d5e213efed4e0ff086eb778f7ec889
Saffat Aziz <saziz028@uottawa.ca>
2020-08-21 15:18:46 -0400

    changes

--- googleclone/src/pages/Home.js
+++ googleclone/src/pages/Home.js
@@ -4,12 +4,17 @@ import {Link} from 'react-router-dom'
 function Home() {
     return (
         <div className ="home">
-            <h1>This is Homepage</h1>
             <div className = "home__header">
             <div className = "home__headerLeft">
-            <Link to = "about">About</Link>
+            <Link to = "/about">About</Link>  
+            {/* Link is better than <a></a> since it does not refresh the page, it simply redirects to the link */}
+            <Link to = "/store">Store</Link>
             </div>
-            <div className = "home__headerRight"></div>
+            <div className = "home__headerRight">
+            <Link to = "/gmail">Gmail</Link>
+            <Link to = "/images">Images</Link>
+
+            </div>
             </div>
             <div className = "home__body"></div>
         </div>
